55a6902ded3a99ad1ab3fc67ae49eafcd192e6c7
Abner0627 <63701415+Abner0627@users.noreply.github.com>
2021-03-23 18:56:21 +0800

    3/23

--- Finding_Lane_Lines_on_the_Road/main.py
+++ Finding_Lane_Lines_on_the_Road/main.py
@@ -105,62 +105,6 @@ def find_lane_pixels(binary_warped):
 
     return leftx, lefty, rightx, righty, out_img
 
-def measure_curvature_meters(binary_warped, left_fitx, right_fitx, ploty):
-    # Define conversions in x and y from pixels space to meters
-    ym_per_pix = 30/720 # meters per pixel in y dimension
-    xm_per_pix = 3.7/700 # meters per pixel in x dimension
-    
-    left_fit_cr = np.polyfit(ploty*ym_per_pix, left_fitx*xm_per_pix, 2)
-    right_fit_cr = np.polyfit(ploty*ym_per_pix, right_fitx*xm_per_pix, 2)
-    # Define y-value where we want radius of curvature
-    # We'll choose the maximum y-value, corresponding to the bottom of the image
-    y_eval = np.max(ploty)
-    
-    # Calculation of R_curve (radius of curvature)
-    left_curverad = ((1 + (2*left_fit_cr[0]*y_eval*ym_per_pix + left_fit_cr[1])**2)**1.5) / np.absolute(2*left_fit_cr[0])
-    right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
-    
-    return left_curverad, right_curverad
-
-def measure_position_meters(binary_warped, left_fit, right_fit):
-    # Define conversion in x from pixels space to meters
-    xm_per_pix = 3.7/700 # meters per pixel in x dimension
-    # Choose the y value corresponding to the bottom of the image
-    y_max = binary_warped.shape[0]
-    # Calculate left and right line positions at the bottom of the image
-    left_x_pos = left_fit[0]*y_max**2 + left_fit[1]*y_max + left_fit[2]
-    right_x_pos = right_fit[0]*y_max**2 + right_fit[1]*y_max + right_fit[2] 
-    # Calculate the x position of the center of the lane 
-    center_lanes_x_pos = (left_x_pos + right_x_pos)//2
-    # Calculate the deviation between the center of the lane and the center of the picture
-    # The car is assumed to be placed in the center of the picture
-    # If the deviation is negative, the car is on the felt hand side of the center of the lane
-    veh_pos = ((binary_warped.shape[1]//2) - center_lanes_x_pos) * xm_per_pix 
-    return veh_pos
-
-def project_lane_info(img, binary_warped, ploty, left_fitx, right_fitx, M_inv, left_curverad, right_curverad, veh_pos):
-    # Create an image to draw the lines on
-    warp_zero = np.zeros_like(binary_warped).astype(np.uint8)
-    color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
-    
-    # Recast the x and y points into usable format for cv2.fillPoly()
-    pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
-    pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
-    pts = np.hstack((pts_left, pts_right))
-    
-    # Draw the lane onto the warped blank image
-    cv2.fillPoly(color_warp, np.int_([pts]), (0,255, 0))
-    
-    # Warp the blank back to original image space using inverse perspective matrix (Minv)
-    newwarp = cv2.warpPerspective(color_warp, M_inv, (img.shape[1], img.shape[0]))
-    
-    # Combine the result with the original image
-    out_img = cv2.addWeighted(img, 1, newwarp, 0.3, 0)
-    
-    
-    return out_img    
-
-
 #%% Load pic 
 P = './data'
 F = 'test02.jpg'
@@ -204,23 +148,8 @@ img_trans, M_inv = _perspective(dst_f, src, dst)
 # plt.tight_layout()
 
 #%% Find Line
-
 leftx, lefty, rightx, righty, out_img = find_lane_pixels(img_trans)
-
-# Fit
-ploty = np.linspace(0, img_trans.shape[0]-1, img_trans.shape[0] )
-left_fit = np.polyfit(lefty, leftx, 2)
-right_fit = np.polyfit(righty, rightx, 2) 
-left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2] 
-right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
-
-# Curve
-left_curverad, right_curverad = measure_curvature_meters(img_trans, left_fitx, right_fitx, ploty)
-
-
-veh_pos = measure_position_meters(img_trans, left_fit, right_fit) 
-out_img = project_lane_info(img, img_trans, ploty, left_fitx, right_fitx, M_inv, left_curverad, right_curverad, veh_pos)
-
+print(leftx)
 
 plt.imshow(out_img)
 plt.show()
