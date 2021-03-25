b6d3442ec9bdd2239da885090fb80bf802f9c35f
Abner0627 <63701415+Abner0627@users.noreply.github.com>
2021-03-26 00:18:58 +0800

    3/26

---
+++ Finding_Lane_Lines_on_the_Road/parameters.py
@@ -0,0 +1,62 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import os
+
+#%% Load video
+P = './data'
+V = 'challenge.mp4'
+cap = cv2.VideoCapture(os.path.join(P, V))
+ 
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    vd = frame
+    break
+fig_0 = plt.figure(0)
+plt.imshow(vd)
+plt.show()
+
+#%% solidWhiteRight.mp4
+'''
+src = np.float32(
+            [[153, 540],  # Bottom left
+             [300, 430],  # Top left
+             [692, 430],  # Top right
+             [872, 540]]) # Bottom right
+
+dst = np.float32(
+            [[153, 540],  # Bottom left
+             [153, 0],  # Top left
+             [872, 0],  # Top right
+             [872, 540]]) # Bottom right 
+'''
+
+#%% solidYellowLeft.mp4
+'''
+src = np.float32(
+            [[100, 540],  # Bottom left
+             [280, 430],  # Top left
+             [675, 430],  # Top right
+             [840, 540]]) # Bottom right
+
+dst = np.float32(
+            [[100, 540],  # Bottom left
+             [100, 0],  # Top left
+             [840, 0],  # Top right
+             [840, 540]]) # Bottom right
+'''
+
+#%% challenge.mp4
+'''
+src = np.float32(
+            [[280, 666],  # Bottom left
+             [503, 517],  # Top left
+             [828, 517],  # Top right
+             [1080, 666]]) # Bottom right
+
+dst = np.float32(
+            [[280, 666],  # Bottom left
+             [280, 0],  # Top left
+             [1080, 0],  # Top right
+             [1080, 666]]) # Bottom right
+'''
